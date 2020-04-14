18e7cc8aa21555df72b69bb023ae63332c4634c8
Hiroki113 <63630530+Hiroki113@users.noreply.github.com>
2020-04-14 16:49:22 +0900

    Add files via upload

---
+++ Scene/SeleScene.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "BaceScene.h"
+#include "../common/Vector2.h"
+
+class SeleScene :
+	public BaceScene
+{
+public:
+	SeleScene();
+	~SeleScene();
+	std::unique_ptr<BaceScene> Update(std::unique_ptr<BaceScene> own) override;
+
+private:
+	short sel;
+	short sel2;
+	Vector2 arrow;
+	bool arrowflag;
+};
+
