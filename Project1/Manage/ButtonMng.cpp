46f9fe3f963f1510b39cac0a4bab461370e5c4a5
Herokuhaku <63630355+Herokuhaku@users.noreply.github.com>
2020-05-15 14:23:58 +0900

    スティックにフラグをつけた メニューでスティックがつかえるようになった

--- Project1/Manage/ButtonMng.cpp
+++ Project1/Manage/ButtonMng.cpp
@@ -25,21 +25,92 @@ std::pair<int, int> ButtonMng::Buttonf(int no,int type)		// ÉRÉìÉgÉçÅ[ÉâÅ[î‘çÜ,É
 	return ButtonState[no][type];
 }
 
+std::pair<int, int> ButtonMng::Thumbf(int no, int type)
+{
+	return ThumbState[no][type];
+}
+
 const void ButtonMng::Buttonf(int no, int type, int First, int Second)
 {
 	ButtonState[no][type] = { First,Second };
 }
 
+const void ButtonMng::Thumbf(int no, int type, int First, int Second)
+{
+	ButtonState[no][type] = {First,Second};
+}
+
+void ButtonMng::ThumbLFlag(void)
+{
+	for (int no = 0;no < 2;no++)
+	{
+		// ç∂ÉXÉeÉBÉbÉNÇÃXé≤
+		if (input[no].ThumbLX > 15000)
+		{
+			ThumbState[no][XINPUT_THUMBL_X].second = ThumbState[no][XINPUT_THUMBL_X].first;
+			ThumbState[no][XINPUT_THUMBL_X].first = 1;
+		}
+		else if (input[no].ThumbLX < -15000)
+		{
+			ThumbState[no][XINPUT_THUMBL_X].second = ThumbState[no][XINPUT_THUMBL_X].first;
+			ThumbState[no][XINPUT_THUMBL_X].first = 2;
+		}
+		else
+		{
+			ThumbState[no][XINPUT_THUMBL_X].second = ThumbState[no][XINPUT_THUMBL_X].first;
+			ThumbState[no][XINPUT_THUMBL_X].first = 0;
+		}
+		// ç∂ÉXÉeÉBÉbÉNÇÃYé≤
+		if (input[no].ThumbLY > 15000)
+		{
+			ThumbState[no][XINPUT_THUMBL_Y].second = ThumbState[no][XINPUT_THUMBL_Y].first;
+			ThumbState[no][XINPUT_THUMBL_Y].first = 2;
+		}
+		else if (input[no].ThumbLY < -15000)
+		{
+			ThumbState[no][XINPUT_THUMBL_Y].second = ThumbState[no][XINPUT_THUMBL_Y].first;
+			ThumbState[no][XINPUT_THUMBL_Y].first = 1;
+		}
+		else
+		{
+			ThumbState[no][XINPUT_THUMBL_Y].second = ThumbState[no][XINPUT_THUMBL_Y].first;
+			ThumbState[no][XINPUT_THUMBL_Y].first = 0;
+		}
+	}
+
+}
+
 void ButtonMng::UpFlag(void)
 {
+	//É{É^Éì
 	for (int i = 0; i < 16; i++)
-	{	
+	{
 		for (int no = 0;no < 2;no++)
 		{
 			ButtonState[no][i].second = ButtonState[no][i].first;
 			ButtonState[no][i].first = input[no].Buttons[i];
 		}
 	}
+	// ÉXÉeÉBÉbÉNX PL1
+	ThumbLFlag();
+	//for (int no;no < 2;no++)
+	//{
+	//	if (input[no].ThumbLX > 15000)
+	//	{
+	//		ThumbState[no][0].second = ThumbState[no][0].first;
+	//		ThumbState[no][0].first = 1;
+	//	}
+	//	else if (input[no].ThumbLX < -15000)
+	//	{
+	//		ThumbState[no][0].second = ThumbState[no][0].first;
+	//		ThumbState[no][0].first = 2;
+	//	}
+	//	else
+	//	{
+	//		ThumbState[no][0].second = ThumbState[no][0].first;
+	//		ThumbState[no][0].first = 0;
+	//	}
+	//}
 }
 void ButtonMng::ButtonInit(void)
 {
@@ -48,6 +119,14 @@ void ButtonMng::ButtonInit(void)
 		for (int i = 0;i < 16;i++)
 		{
 			ButtonState[x][i] = { 0, 0 };
+		}
+	}
+	//Å@ÉXÉeÉBÉbÉNÇÃÉXÉeÅ[É^ÉXÇÕ0
+	for (int y=0;y < 2;y++)
+	{
+		for (int x=0;x < 4;x++)
+		{
+			ThumbState[y][x] = { 0,0 };
 		}
 	}
 }
