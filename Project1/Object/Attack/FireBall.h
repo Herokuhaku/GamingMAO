4e87fee0b6244fda88ff8a15dbbc7a66704ae654
reo-prg <59633704+reo-prg@users.noreply.github.com>
2020-05-22 11:33:43 +0900

    火の玉

--- Project1/Object/Attack/FireBall.h
+++ Project1/Object/Attack/FireBall.h
@@ -1,19 +1,23 @@
 #pragma once
 #include "../Object.h"
+#include "../../func/CheckHitStage.h"
 
 class FireBall :
 	public Object
 {
 public:
 	FireBall();
-	FireBall(Vector2 pos, DIR dir, int speed, TIME time, int stage);
+	FireBall(Vector2 pos, DIR dir, int speed, TIME time, int stage, OBJ_TYPE target);
 	~FireBall();
 
 	void Update(void)override;
+
+	void IfHitAttack(void)override;
 
 private:
 	void Init(void);
 
 	int _speed;
+	OBJ_TYPE _target;
 };
 
